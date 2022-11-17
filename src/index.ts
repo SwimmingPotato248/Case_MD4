8032061de09c8c2040826bdb227fb1ad85672bbe
SwimmingPotato <ptphong200i@gmail.com>
2022-11-17 08:51:42 +0700

    Add database connection

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express, { Request, Response } from "express";
 import * as dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+
+mongoose.connect(process.env.DATABASE_URL!, err => {
+  if (err) console.error(err);
+  console.log("Connect success");
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello");
