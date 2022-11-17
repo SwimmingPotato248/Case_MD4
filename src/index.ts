d5bbabf5adff7f4d27baab917d97395c6829c29f
SwimmingPotato <ptphong200i@gmail.com>
2022-11-17 08:44:53 +0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
